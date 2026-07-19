7a5c9190bdf292b00438ba576017d645791fe34d
EnglishBeach <kraynoff.i@yandex.ru>
2026-07-19 16:11:08 +0300

    module_structure: FIX private lazyimport and version get

--- src/simdel/__init__.py
+++ src/simdel/__init__.py
@@ -1,11 +1,11 @@
 """A collection of Gromacs manipulation utilities and MD-based pipelines."""
 
-from importlib.metadata import version
+import importlib.metadata as _meta
 
-import lazyimports
+import lazyimports as _lazyimports
 
-with lazyimports.lazy_imports("simdel._wrappers", "plumed", "lomap"):
+with _lazyimports.lazy_imports("simdel._wrappers", "plumed", "lomap"):
     from . import analyse, chem, func, run, sim, traj
     from ._utils import STRICT
 
-__version__ = version("simdel")
+__version__ = _meta.version("simdel")
