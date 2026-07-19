ae81b8645cbd36c26fb60c527265482eb084ad0a
EnglishBeach <kraynoff.i@yandex.ru>
2026-07-19 14:51:55 +0300

    module_structure: UPD version

--- src/simdel/__init__.py
+++ src/simdel/__init__.py
@@ -1,8 +1,11 @@
 """A collection of Gromacs manipulation utilities and MD-based pipelines."""
 
+from importlib.metadata import version
+
 import lazyimports
 
 with lazyimports.lazy_imports("simdel._wrappers", "plumed", "lomap"):
     from . import analyse, chem, func, run, sim, traj
     from ._utils import STRICT
-    from .version import VERSION as __version__
+
+__version__ = version("simdel")
